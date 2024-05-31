96a6a71aa89a2796f0e497cce9998705b308e3ce
CrozzDev <crozzdev95@outlook.com>
2024-05-30 22:55:31 -0500

    started js script, fixed some improvements in html and css

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
-import './style.css'
+const $drumPads = document.getElementsByClassName('drum-pad')
+const $powerToggle = document.getElementById('power-switch')
+const $bankToggle = document.getElementById('bank-switch')
+const $volumeSlider = document.getElementById('volume-slider')
+const $display = document.getElementById('display')
 
